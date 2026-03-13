fd438445fd4f7e4d95ece814fbe3f70f3f197be6
Treetrampler <treetrampler@gmail.com>
2026-03-14 06:17:14 +1100

    kys kevin i aint listening to ur biomes, anyways heres the schema

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -1,3 +1,67 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
-export default defineSchema({});
+export default defineSchema({
+  users: defineTable({
+
+    userName: v.string(),
+    email: v.string(),
+    password: v.string(),
+
+    elo: v.number(),
+    streak: v.number(),
+
+    allTimeWins: v.number(),
+    allTimeLosses: v.number(),
+
+    dailyDistance: v.number(),
+    weeklyDistance: v.number(),
+    monthlyDistance: v.number(),
+
+    dailySpeed: v.number(),
+    weeklySpeed: v.number(),
+    monthlySpeed: v.number(),
+
+  }),
+
+  liveRaces: defineTable({
+    raceId: v.string(),
+
+    userAId: v.id("users"),
+    userBId: v.id("users"),
+
+    userADistance: v.number(),
+    userBDistance: v.number(),
+
+    raceDistance: v.number(),
+  }),
+
+  finishedRaces: defineTable({
+    userId: v.id("users"),
+    raceId: v.string(),
+
+    time: v.number(),
+    distance: v.number(),
+    pace: v.number(),
+  }),
+
+  friends: defineTable({
+    aFriendId: v.id("users"),
+    bFriendId: v.id("users"),
+
+    h2hAWins: v.number(),
+    h2hBWins: v.number(),
+  }),
+
+  groups: defineTable({
+    dateCreated: v.number(),
+
+    weeklyDistanceGoal: v.number(),
+    monthlyDistanceGoal: v.number(),
+  }),
+
+  groupLinking: defineTable({
+    userId: v.id("users"),
+    groupId: v.id("groups"),
+  }),
+});
